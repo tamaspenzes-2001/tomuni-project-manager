c0439555f8afb7bdc9d82409ff6d32bcd50c767f
tamaspenzes-2001 <penzestamas@proton.me>
2026-04-16 17:32:36 +0200

    implement TemplatesMenuEntry

--- UI/Sidebar/TemplatesMenuEntry.py
+++ UI/Sidebar/TemplatesMenuEntry.py
@@ -1,5 +1,23 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout
+import qtawesome as qta
 
 class TemplatesMenuEntry(QWidget):
     def __init__(self, text):
         super().__init__()
+        self.name = QLabel(text)
+        self.deleteButton = QPushButton()
+        self.deleteButton.setIcon(qta.icon("ri.delete-bin-6-line"))
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.name)
+        self.layout.addWidget(self.deleteButton)
+        self.setLayout(self.layout)
+
+        self.mouseReleaseEvent=self.openTemplate
+        self.deleteButton.clicked.connect(self.deleteTemplate)
+
+    def openTemplate(self, event):
+        pass
+
+    def deleteTemplate(self):
+        self.sender().parent().deleteLater()
