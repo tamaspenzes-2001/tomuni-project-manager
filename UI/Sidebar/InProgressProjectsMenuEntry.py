49634f86ad6411079d9cb2f207cf1df53ffb24c4
tamaspenzes-2001 <penzestamas@proton.me>
2026-04-22 16:19:47 +0200

    implement InProgressProjectsMenuEntry

--- UI/Sidebar/InProgressProjectsMenuEntry.py
+++ UI/Sidebar/InProgressProjectsMenuEntry.py
@@ -1,10 +1,23 @@
-from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout
+import qtawesome as qta
 
 class InProgressProjectsMenuEntry(QWidget):
     def __init__(self, name):
         super().__init__()
-        print("Hello")
         self.name = QLabel(name)
+        self.completeButton = QPushButton()
+        self.completeButton.setIcon(qta.icon("fa5s.check"))
+
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.name)
+        self.layout.addWidget(self.completeButton)
         self.setLayout(self.layout)
+
+        self.mouseReleaseEvent = self.openProject
+        self.completeButton.clicked.connect(self.completeProject)
+
+    def openProject(self):
+        pass
+
+    def completeProject(self):
+        pass
