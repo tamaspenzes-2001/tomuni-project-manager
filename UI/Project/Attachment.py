4c764104bdbfca14cbfaaa4b3fdcaa96aeab171c
tamaspenzes-2001 <penzestamas@proton.me>
2026-04-29 15:38:47 +0200

    implement TaskArtifactSection

---
+++ UI/Project/Attachment.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import QWidget
+
+class Attachment(QWidget):
+    def __init__(self, filePath: str):
+        super().__init__()
--- UI/Project/TaskArtifactSection.py
+++ UI/Project/TaskArtifactSection.py
@@ -1,6 +1,28 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from superqt import QFlowLayout
 from typing import List
+from UI.Project.Attachment import Attachment
 
 class TaskArtifactSection(QWidget):
     def __init__(self, filePaths: List[str], templates: bool = False):
         super().__init__()
+        self.label = QLabel("Artifact templates" if templates else "Artifacts")
+        self.labelLayout = QVBoxLayout()
+        self.labelLayout.addWidget(self.label)
+        self.labelLayout.addStretch()
+
+        self.addAttachmentButton = QPushButton("+")
+        self.addAttachmentButton.clicked.connect(self.addAttachment)
+        self.attachmentsLayout = QFlowLayout()
+        for path in filePaths:
+            self.attachmentsLayout.addWidget(Attachment(path))
+        self.attachmentsLayout.addWidget(self.addAttachmentButton)
+        
+        self.layout = QHBoxLayout()
+        self.layout.addLayout(self.labelLayout)
+        self.layout.addLayout(self.attachmentsLayout)
+        self.layout.addStretch()
+        self.setLayout(self.layout)
+
+    def addAttachment(self):
+        pass
